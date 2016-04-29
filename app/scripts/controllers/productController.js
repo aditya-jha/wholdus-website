9e51c44cf66e67fdce3ec1cd9766e6cd84fa290f
Aditya Jha <batu.aditya007@gmail.com>
2016-04-29 23:16:43 +0530

    changes

--- app/scripts/controllers/productController.js
+++ app/scripts/controllers/productController.js
@@ -8,6 +8,43 @@
         'UtilService',
         function($scope, $routeParams, $log, APIService, UtilService) {
 
+            function praseProductDetails(p) {
+                $scope.productDetailsKeys = [{
+                    label: 'Brand',
+                    value: p.details.brand
+                }, {
+                    label: 'Pattern',
+                    value: p.details.pattern
+                }, {
+                    label: 'Style',
+                    value: p.details.style
+                }, {
+                    label: 'Fabric/GSM',
+                    value: p.details.fabric_gsm
+                }, {
+                    label: 'Sleeve',
+                    value: p.details.sleeve
+                }, {
+                    label: 'Neck/Collar',
+                    value: p.details.neck_collar_type
+                }, {
+                    label: 'Length',
+                    value: p.details.length
+                }, {
+                    label: 'Work Decor',
+                    value: p.details.work_decoration_type
+                }, {
+                    label: 'Colors',
+                    value: p.details.colours
+                }, {
+                    label: 'Sizes',
+                    value: p.details.sizes
+                }, {
+                    label: 'Features',
+                    value: p.details.special_feature
+                }];
+            }
+
             function getProducts() {
                 var params = {
                     productID: UtilService.getIDFromSlug($routeParams.product)
@@ -16,6 +53,7 @@
                 .then(function(response) {
                     if(response.products.length) {
                         $scope.product = response.products[0];
+                        praseProductDetails($scope.product);
                     }
                 }, function(error) {
                     $scope.product = [];
