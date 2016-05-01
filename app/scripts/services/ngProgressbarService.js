f4c36479f91123bbe6c0cd632d8279f7646d185d
Aditya Jha <batu.aditya007@gmail.com>
2016-05-01 09:36:13 +0530

    changes

--- app/scripts/services/ngProgressbarService.js
+++ app/scripts/services/ngProgressbarService.js
@@ -4,29 +4,36 @@
     webapp.factory("ngProgressBarService", [
         '$rootScope',
         'ngProgressFactory',
-        function($rootScope, ngProgressFactory) {
+        '$timeout',
+        function($rootScope, ngProgressFactory, $timeout) {
             var factory = {};
 
             var settings = {
                 color: 'red',
                 height: '3px',
             };
-            var progressbar;
+            var progressbar, element;
 
             var init = function() {
                 progressbar = ngProgressFactory.createInstance();
                 progressbar.setColor(settings.color);
                 progressbar.setHeight(settings.height);
-                progressbar.setParent(document.getElementById("progressBar"));
+                element = document.getElementById("progressBar");
+                progressbar.setParent(element);
+                element.style.display = 'none';
             };
             init();
 
             $rootScope.$on('showProgressbar', function(event) {
+                element.style.display = 'block';
                 progressbar.start();
             });
 
             $rootScope.$on('endProgressbar', function(event) {
                 progressbar.complete();
+                $timeout(function() {
+                    element.style.display = 'none';
+                }, 1000);
             });
 
             return factory;
