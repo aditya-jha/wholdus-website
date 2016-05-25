3cf07e638117b1da69863aad0f83323c5c4a1cda
Aditya Jha <batu.aditya007@gmail.com>
2016-05-25 17:28:15 +0530

    sidenav chagnes

--- app/scripts/directives/wuHeader.js
+++ app/scripts/directives/wuHeader.js
@@ -3,18 +3,10 @@
         return {
             restrict: 'AE',
             templateUrl: 'views/directives/wuHeader.html',
-            scope: {},
-            link: function(scope, element, attributes) {},
             controller: [
                 '$scope',
                 '$rootScope',
-                '$log',
-                function($scope, $rootScope, $log) {
-                    $log.log("wuheader directive loaded");
-
-                    $scope.catSidenav = function() {
-                        $rootScope.$broadcast('catSidenav');
-                    };
+                function($scope, $rootScope) {
                 }
             ]
         };
