8dae2becdbe73987f98f2aaec1f2f9f98c1a6140
Aishwarya Mittal <aishwaryamittal2011@gmail.com>
2016-06-29 17:08:08 +0530

    Added image loader in buyer panel

--- app/scripts/controllers/feedController.js
+++ app/scripts/controllers/feedController.js
@@ -20,6 +20,8 @@
             };
             $scope.products = [];
             $scope.productToShow = null;
+            $scope.displayImageLoading=true;
+            $scope.displayImageStyle={'opacity':'1.0'};
 
             function parseSearchParams() {
                 $scope.noProducts = false;
@@ -124,6 +126,8 @@
                     });
                     $scope.pageSettings.productIndex += 1;
                     if($scope.pageSettings.productIndex < $scope.products.length) {
+                        $scope.displayImageLoading=true;
+                          $scope.displayImageStyle={'opacity':'0.5'};
                         setProductToShow($scope.pageSettings.productIndex);
                     } else {
                         if($scope.pageSettings.currentPage < $scope.pageSettings.totalPages) {
@@ -141,11 +145,16 @@
                 }
             };
 
+             $scope.imageLoaded= function(){
+                    $scope.displayImageLoading=false;
+                    $scope.displayImageStyle={'opacity':'1.0'};
+            };
+
             $scope.$on('$destroy', function() {
                 angular.forEach(listeners, function(value) {
                     if(value) value();
                 });
             });
         }
-    ]);
+        ]);
 })();
