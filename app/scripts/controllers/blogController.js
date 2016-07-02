90224b3ea7f1249a37ff76dfb259bd38f77d7f68
Aditya Jha <batu.aditya007@gmail.com>
2016-07-02 17:36:05 +0530

    blog

--- app/scripts/controllers/blogController.js
+++ app/scripts/controllers/blogController.js
@@ -3,8 +3,22 @@
         '$scope',
         '$log',
         '$routeParams',
-        function($scope, $log, $routeParams) {
-            $log.log($routeParams);
+        '$sce',
+        function($scope, $log, $routeParams, $sce) {
+
+            function init() {
+                if($routeParams.article && $routeParams.article == 'home') {
+                    
+                }
+            }
+            $scope.article = {
+                title: 'Blog post 1 Blog post 1 Blog post 1 Blog post 1 Blog post 1 Blog post 1 Blog post 1',
+                author: 'Aditya Jha',
+                url: '/blog/new',
+                converImage: 'https://media.licdn.com/mpr/mpr/jc/AAEAAQAAAAAAAAdZAAAAJDE1YmU1NDRkLWI2ZGMtNGU4MC1hN2EyLWNhNDM4MTg2YzA0OA.jpg',
+                content: $sce.trustAsHtml('<b>this is content</b>'),
+                created_at: '1467387369461'
+            };
         }
     ]);
 })();
