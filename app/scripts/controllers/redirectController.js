163c5b93bd5a8bdc817ce876a65e22f0803a5571
Aditya Jha <batu.aditya007@gmail.com>
2016-07-22 08:47:59 +0530

    refactored

--- app/scripts/controllers/redirectController.js
+++ app/scripts/controllers/redirectController.js
@@ -24,24 +24,27 @@
                 APIService.apiCall('POST', APIService.getAPIUrl('buyerProductsLanding'), {"buyerproductID":buyerproductID});
             }
 
-            if($routeParams.uniqueUrl) {
-                var uniqueUrl = $routeParams.uniqueUrl.split('-');
-                if(uniqueUrl.length == 3) {
-                    buyerproductID = uniqueUrl[2];
-                    buyer_panel_url = uniqueUrl[0] + '-' + uniqueUrl[1];
-                    setCorrectPage(buyer_panel_url, buyerproductID);
+            function init() {
+                if($routeParams.uniqueUrl) {
+                    var uniqueUrl = $routeParams.uniqueUrl.split('-');
+                    if(uniqueUrl.length == 3) {
+                        buyerproductID = uniqueUrl[2];
+                        buyer_panel_url = uniqueUrl[0] + '-' + uniqueUrl[1];
+                        setCorrectPage(buyer_panel_url, buyerproductID);
+                    } else {
+                        $location.url('/account/hand-picked-products');
+                    }
                 } else {
-                    $location.url('/account/hand-picked-products');
-                }
-            } else {
-                var url = $location.url();
-                if(url.indexOf('blog') >= 0) {
-                    $location.url('/blog/home');
-                } else if(url.indexOf('bp') >= 0) {
-                    $location.url('/account/hand-picked-products');
+                    var url = $location.url();
+                    if(url.indexOf('blog') >= 0) {
+                        $location.url('/blog/home');
+                    } else if(url.indexOf('bp') >= 0) {
+                        $location.url('/account/hand-picked-products');
+                    }
                 }
             }
 
+            init();
         }
     ]);
 })();
