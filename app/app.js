005f4ee102e3de5148fd74ac883a863f834a6ba8
Aditya Jha <batu.aditya007@gmail.com>
2016-07-30 17:16:47 +0530

    turning debug info off

--- app/app.js
+++ app/app.js
@@ -12,66 +12,69 @@ webapp.config([
     '$mdThemingProvider',
     '$mdIconProvider',
     'localStorageServiceProvider',
-     function($routeProvider, $locationProvider, $mdThemingProvider, $mdIconProvider, localStorageServiceProvider, $location) {
+    '$compileProvider',
+     function($routeProvider, $locationProvider, $mdThemingProvider, $mdIconProvider, localStorageServiceProvider, $location, $compileProvider) {
 
-        $routeProvider.when('/', {
-            templateUrl: "views/homepage.html",
-            controller: "HomeController"
-        }).when('/404', {
-            templateUrl: 'views/404.html',
-            controller: "StaticPagesController"
-        }).when('/aboutus', {
-            templateUrl: "views/aboutus.html",
-            controller: "StaticPagesController"
-        }).when('/contactus', {
-            templateUrl: "views/contactus.html",
-            controller: "StaticPagesController"
-        }).when('/faq',{
-            templateUrl: "views/faq.html",
-            controller: "StaticPagesController"
-        }).when('/return-refund-policy', {
-            templateUrl: "views/returnRefund.html",
-            controller: "StaticPagesController"
-        }).when('/we-are-social', {
-            templateUrl: "views/we-are-social.html",
-            controller: "StaticPagesController",
-        }).when('/account/orders', {
-            templateUrl: 'views/account/orders.html',
-            controller: 'OrdersController'
-        }).when('/account/profile', {
-            templateUrl: 'views/account/profile.html',
-            controller: 'ProfileController'
-        }).when('/account/hand-picked-products', {
-            templateUrl: 'views/account/feed.html',
-            controller: 'FeedController',
-            reloadOnSearch: false
-        }).when('/blog', {
-            template: '<div></div>',
-            controller: 'RedirectController'
-        }).when('/blog/:article', {
-            templateUrl: 'views/blog/blogPage.html',
-            controller: 'BlogController'
-        }).when('/bp', {
-            template: '<div></div>',
-            controller: 'RedirectController'
-        }).when('/bp/:uniqueUrl', {
-            template: '<div></div>',
-            controller: 'RedirectController'
-        }).when('/:category', {
-            templateUrl: "views/categorypage.html",
-            controller: "CategoryController",
-        }).when('/:category/:product', {
-            templateUrl: "views/productpage.html",
-            controller: "ProductController"
-        });
+         $compileProvider.debugInfoEnabled(false);
+         
+         $routeProvider.when('/', {
+             templateUrl: "views/homepage.html",
+             controller: "HomeController"
+         }).when('/404', {
+             templateUrl: 'views/404.html',
+             controller: "StaticPagesController"
+         }).when('/aboutus', {
+             templateUrl: "views/aboutus.html",
+             controller: "StaticPagesController"
+         }).when('/contactus', {
+             templateUrl: "views/contactus.html",
+             controller: "StaticPagesController"
+         }).when('/faq',{
+             templateUrl: "views/faq.html",
+             controller: "StaticPagesController"
+         }).when('/return-refund-policy', {
+             templateUrl: "views/returnRefund.html",
+             controller: "StaticPagesController"
+         }).when('/we-are-social', {
+             templateUrl: "views/we-are-social.html",
+             controller: "StaticPagesController",
+         }).when('/account/orders', {
+             templateUrl: 'views/account/orders.html',
+             controller: 'OrdersController'
+         }).when('/account/profile', {
+             templateUrl: 'views/account/profile.html',
+             controller: 'ProfileController'
+         }).when('/account/hand-picked-products', {
+             templateUrl: 'views/account/feed.html',
+             controller: 'FeedController',
+             reloadOnSearch: false
+         }).when('/blog', {
+             template: '<div></div>',
+             controller: 'RedirectController'
+         }).when('/blog/:article', {
+             templateUrl: 'views/blog/blogPage.html',
+             controller: 'BlogController'
+         }).when('/bp', {
+             template: '<div></div>',
+             controller: 'RedirectController'
+         }).when('/bp/:uniqueUrl', {
+             template: '<div></div>',
+             controller: 'RedirectController'
+         }).when('/:category', {
+             templateUrl: "views/categorypage.html",
+             controller: "CategoryController",
+         }).when('/:category/:product', {
+             templateUrl: "views/productpage.html",
+             controller: "ProductController"
+         });
 
-        $routeProvider.otherwise('/');
-        $locationProvider.html5Mode(true);
+         $routeProvider.otherwise('/');
+         $locationProvider.html5Mode(true);
 
-        $mdThemingProvider.theme('default')
-                        .primaryPalette('deep-purple')
-                        .accentPalette('deep-orange');
+         $mdThemingProvider.theme('default')
+                         .primaryPalette('deep-purple')
+                         .accentPalette('deep-orange');
 
-        localStorageServiceProvider.setPrefix('probzip-webapp');
+         localStorageServiceProvider.setPrefix('probzip-webapp');
     }
 ]);
