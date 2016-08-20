1e69d25b3b50b6b742a6817399e877db7c15da09
Aditya Jha <batu.aditya007@gmail.com>
2016-08-20 22:18:59 +0530

    product card without shorlist feature done

--- app/scripts/directives/wuProduct.js
+++ app/scripts/directives/wuProduct.js
@@ -8,6 +8,32 @@
             },
             replace: true,
             templateUrl: 'views/directives/wuProduct.html',
+            controller: [
+                '$scope',
+                '$log',
+                function($scope, $log) {
+
+                    var showShortlist = function(type) {
+                        if(type==1) {
+                            $scope.shortlisted = true;
+                        } else {
+                            $scope.shortlisted = false;
+                        }
+                    };
+
+                    $scope.toggleShortlist = function(ev) {
+                        ev.preventDefault();
+                        ev.stopPropagation();
+                    };
+
+                    function init() {
+                        $scope.shortlisted = false;
+                        $scope.showShortlist = showShortlist;
+                    }
+
+                    init();
+                }
+            ]
         };
     });
 })();
